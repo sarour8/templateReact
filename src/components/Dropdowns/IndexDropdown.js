0bfc3b827b0aa70cf5b0f5bbb99ad78b91094be6
sarra elmaher <elmahersarra@gmail.com>
2025-11-26 20:40:53 +0100

    Update IndexDropdown.js

--- src/components/Dropdowns/IndexDropdown.js
+++ src/components/Dropdowns/IndexDropdown.js
@@ -35,8 +35,19 @@ const IndexDropdown = () => {
           (dropdownPopoverShow ? "block " : "hidden ") +
           "bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
         }
-      >
-        <span
+      ><Link
+          to="/auth/login"
+          className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+        >
+          Login
+        </Link>
+        <Link
+          to="/auth/register"
+          className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+        >
+          Register
+        </Link>
+        {/*<span
           className={
             "text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400"
           }
@@ -75,18 +86,7 @@ const IndexDropdown = () => {
         >
           Auth Layout
         </span>
-        <Link
-          to="/auth/login"
-          className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-        >
-          Login
-        </Link>
-        <Link
-          to="/auth/register"
-          className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-        >
-          Register
-        </Link>
+        
         <div className="h-0 mx-4 my-2 border border-solid border-blueGray-100" />
         <span
           className={
@@ -112,7 +112,7 @@ const IndexDropdown = () => {
           className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
         >
           Profile
-        </Link>
+        </Link>*/}
       </div>
     </>
   );
